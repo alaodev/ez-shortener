f4717f2e0a89ccaf50d7c6ccbfe5d1eb5ad0808b
alaodev <alaodev@gmail.com>
2025-03-07 13:30:07 -0300

    chore(apps/authentication): setup users database connection

--- apps/authentication/src/app.module.ts
+++ apps/authentication/src/app.module.ts
@@ -1,12 +1,24 @@
 import * as path from 'path';
+import { DatabasesModule } from '@ez-shortener/databases';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: path.resolve(__dirname, '..', '..', '..', '.env'),
+    }),
+    DatabasesModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGO_USERS_URI');
+        if (!uri) throw new Error('MONGO_USERS_URI must be provided');
+        return {
+          uri,
+          dbName: 'users',
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
 })
