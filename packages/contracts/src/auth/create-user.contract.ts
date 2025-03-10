56965edbb58e2f69e54d3619c230e58d2cc2abd0
alaodev <alaodev@gmail.com>
2025-03-10 16:06:06 -0300

    chore(packages/contracts): implements CreateUserContract

---
+++ packages/contracts/src/auth/create-user.contract.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+export const createUserSchema = z
+  .object({
+    username: z
+      .string({ message: 'field is required' })
+      .min(3, { message: 'field must contain at least 3 characters' })
+      .max(32, { message: 'field must contain a maximum of 32 characters' }),
+    email: z
+      .string({ message: 'field is required' })
+      .email({ message: 'invalid email' }),
+    password: z
+      .string({ message: 'field is required' })
+      .min(8, { message: 'field must contain at least 5 characters' })
+      .max(32, { message: 'field must contain a maximum of 32 characters' })
+      .regex(/[a-z]/, 'field must contain at least one lowercase letter')
+      .regex(/[A-Z]/, 'field must contain at least one uppercase letter')
+      .regex(/\d/, 'field must contain at least one number')
+      .regex(/[\W_]/, 'field must contain at least one special character'),
+    passwordConfirmation: z.string({
+      message: 'field is required',
+    }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'field must match (password)',
+    path: ['passwordConfirmation'],
+  });
+
+export type CreateUserContract = z.infer<typeof createUserSchema>;
