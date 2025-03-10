294b7e30bdb6305c052678cde151de00ca9fe5ff
alaodev <alaodev@gmail.com>
2025-03-10 20:26:43 -0300

    feat(apps/authentication): provides and exports FindUserByEmailUseCase on UsersModule

--- apps/authentication/src/modules/users/users.module.ts
+++ apps/authentication/src/modules/users/users.module.ts
@@ -1,6 +1,7 @@
 import { MongooseModule } from '@ez-shortener/databases/nestjs-mongoose';
 import { Module } from '@nestjs/common';
 import { CreateUserUseCaseImpl } from './application/use-cases/create-user.usecase.impl';
+import { FindUserByEmailUseCaseImpl } from './application/use-cases/find-user-by-email.usecase.impl';
 import { User, UserSchema } from './infrastructure/schemas/user.schema';
 import { UserMongoRepository } from './infrastructure/repositories/user-mongo.repository';
 import { BcryptEncryptionService } from '../../common/infrastructure/services/bcrypt-encryption.service';
@@ -11,9 +12,10 @@ import { BcryptEncryptionService } from '../../common/infrastructure/services/bc
   ],
   providers: [
     { provide: 'CreateUserUseCase', useClass: CreateUserUseCaseImpl },
+    { provide: 'FindUserByEmailUseCase', useClass: FindUserByEmailUseCaseImpl },
     { provide: 'UserRepository', useClass: UserMongoRepository },
     { provide: 'EncryptionService', useClass: BcryptEncryptionService },
   ],
-  exports: ['CreateUserUseCase'],
+  exports: ['CreateUserUseCase', 'FindUserByEmailUseCase'],
 })
 export class UsersModule {}
