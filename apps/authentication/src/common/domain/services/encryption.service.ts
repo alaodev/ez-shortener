112262e972638b41912631b9571e13f30ef95718
alaodev <alaodev@gmail.com>
2025-03-10 13:57:51 -0300

    feat(apps/authentication): implements common EncryptionService compare method

--- apps/authentication/src/common/domain/services/encryption.service.ts
+++ apps/authentication/src/common/domain/services/encryption.service.ts
@@ -1,3 +1,4 @@
 export abstract class EncryptionService {
   abstract hash(value: string): Promise<string>;
+  abstract compare(value: string, encrypted: string): Promise<boolean>;
 }
--- apps/authentication/src/common/infrastructure/services/bcrypt-encryption.service.ts
+++ apps/authentication/src/common/infrastructure/services/bcrypt-encryption.service.ts
@@ -5,4 +5,8 @@ export class BcryptEncryptionService implements EncryptionService {
   async hash(value: string, salt: number = 10): Promise<string> {
     return bcrypt.hash(value, salt);
   }
+
+  async compare(value: string, encrypted: string): Promise<boolean> {
+    return bcrypt.compare(value, encrypted);
+  }
 }
