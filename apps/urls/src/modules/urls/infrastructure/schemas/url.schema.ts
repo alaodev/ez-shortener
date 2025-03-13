66f8043f6fb80b9e0f2cbe538278212bc86723b8
alaodev <alaodev@gmail.com>
2025-03-13 10:16:28 -0300

    feat(apps/urls): implements UrlsSchema

---
+++ apps/urls/src/modules/urls/infrastructure/schemas/url.schema.ts
@@ -0,0 +1,22 @@
+import {
+  Prop,
+  Schema,
+  SchemaFactory,
+} from '@ez-shortener/databases/nestjs-mongoose';
+import { HydratedDocument, Types } from '@ez-shortener/databases/mongoose';
+
+export type UrlDocument = HydratedDocument<Url>;
+
+@Schema()
+export class Url {
+  @Prop({ required: true })
+  originalUrl: string;
+
+  @Prop({ required: true })
+  shortId: string;
+
+  @Prop({ required: true, type: Types.ObjectId })
+  owner: string;
+}
+
+export const UrlSchema = SchemaFactory.createForClass(Url);
