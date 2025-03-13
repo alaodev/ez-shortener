2e70e871ad42d16b726b0005752151be620ae276
alaodev <alaodev@gmail.com>
2025-03-13 10:31:35 -0300

    feat(apps/urls): imports ConfigModule on AppModule

--- apps/urls/src/app.module.ts
+++ apps/urls/src/app.module.ts
@@ -1,7 +1,15 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UrlsModule } from './modules/urls/urls.module';
 
 @Module({
-  imports: [UrlsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(__dirname, '..', '..', '..', '.env'),
+    }),
+    UrlsModule,
+  ],
 })
 export class AppModule {}
