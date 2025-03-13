fd0d77622a6e1c9e64c47c3a1dbaabe17ae6fe55
alaodev <alaodev@gmail.com>
2025-03-13 10:11:22 -0300

    feat(apps/urls): defines domain use-cases layer

--- apps/urls/src/modules/urls/domain/use-cases/shorten-url.usecase.ts
+++ apps/urls/src/modules/urls/domain/use-cases/shorten-url.usecase.ts
@@ -1,3 +1,6 @@
+import { ShortenUrlOutput } from '../types/output/use-cases/shorten-url.output';
+import { ShortenUrlInput } from '../types/inputs/use-cases/shorten-url.input';
+
 export abstract class ShortenUrlUseCase {
-  abstract execute(originalUrl: string): Promise<string>;
+  abstract execute(data: ShortenUrlInput): Promise<ShortenUrlOutput>;
 }
