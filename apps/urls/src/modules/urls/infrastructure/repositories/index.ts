b9df03424c0c24b41371a05fa0634dce5b1e76a6
alaodev <alaodev@gmail.com>
2025-03-13 10:18:00 -0300

    feat(apps/urls): implements MongoFindUrlByShortIdRepository

---
+++ apps/urls/src/modules/urls/infrastructure/repositories/index.ts
@@ -0,0 +1,2 @@
+export * from './mongo-create-url.repository';
+export * from './mongo-find-url-by-short-url.repository';
---
+++ apps/urls/src/modules/urls/infrastructure/repositories/mongo-find-url-by-short-url.repository.ts
@@ -0,0 +1,26 @@
+import { Model } from '@ez-shortener/databases/mongoose';
+import { InjectModel } from '@ez-shortener/databases/nestjs-mongoose';
+import { Injectable } from '@nestjs/common';
+import { Url as UrlSchema } from '../schemas/url.schema';
+import { Url } from '../../domain/entities/url.entity';
+import { FindUrlByShortIdRepository } from '../../domain/repositories/find-url-by-short-url.repository';
+import { FindUrlByShortIdRepositoryOutput } from '../../domain/types/output/repositories/find-url-by-short-url-repository.output';
+
+@Injectable()
+export class MongoFindUrlByShortIdRepository
+  implements FindUrlByShortIdRepository
+{
+  constructor(@InjectModel(UrlSchema.name) private urlModel: Model<Url>) {}
+
+  async findUrlByShortId(
+    shortId: string,
+  ): Promise<FindUrlByShortIdRepositoryOutput | null> {
+    const foundUrl = await this.urlModel.findOne({ shortId });
+    if (!foundUrl) return null;
+    return {
+      id: foundUrl._id.toString(),
+      originalUrl: foundUrl.originalUrl,
+      shortId: foundUrl.shortId,
+    };
+  }
+}
