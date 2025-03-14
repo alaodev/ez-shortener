20832fd84fc563173c9900d2ed51fbb3a032c270
alaodev <alaodev@gmail.com>
2025-03-14 09:34:35 -0300

    chore(apps/authentication): removes unnecessary exports files

--- apps/authentication/src/common/domain/services/index.ts
+++
@@ -1,2 +0,0 @@
-export * from '../../../modules/users/domain/services/hashing.service';
-export * from '../../../modules/auth/domain/services/compare.service';
--- apps/authentication/src/common/infrastructure/services/index.ts
+++
@@ -1,2 +0,0 @@
-export * from '../../../modules/users/infrastructure/services/bcrypt-hashing.service';
-export * from '../../../modules/auth/infrastructure/bcrypt-compare.service';
