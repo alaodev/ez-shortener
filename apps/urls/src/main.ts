4ee7eed3fa6a70ba8d8f1a05f81e5014bd88adfb
alaodev <alaodev@gmail.com>
2025-03-16 23:27:59 -0300

    chore(apps): setup applications ports

--- apps/urls/src/main.ts
+++ apps/urls/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Urls');
+  const port = process.env.URLS_PORT;
+  if (!port) {
+    logger.error('MONGO_USERS_URI must be provided');
+    process.exit(1);
+  }
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(port, () => logger.log(`Server running on port ${port}`));
 }
 bootstrap();
