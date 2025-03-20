fbad4d153e2e27ce41214edcb3a5c95e83a8193b
alaodev <alaodev@gmail.com>
2025-03-20 16:49:20 -0300

    feat(app/web): implements SigupForm component

---
+++ apps/web/components/forms/SignupForm.vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+import * as zod from 'zod';
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+
+export type SubmitFormDataType = zod.infer<typeof signupSchemaValidator>;
+
+const emits = defineEmits<{
+  (event: 'submit:form', data: SubmitFormDataType): void;
+}>();
+
+const signupSchemaValidator = zod
+  .object({
+    username: zod.string().min(3).max(32),
+    email: zod.string().email().nonempty(),
+    password: zod
+      .string()
+      .min(8)
+      .max(32)
+      .regex(/[a-z]/)
+      .regex(/[A-Z]/)
+      .regex(/\d/)
+      .regex(/[\W_]/),
+    passwordConfirmation: zod.string().nonempty(),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'field must match (password)',
+    path: ['passwordConfirmation'],
+  });
+
+const formSchema = toTypedSchema(signupSchemaValidator);
+
+const { handleSubmit } = useForm({
+  validationSchema: formSchema,
+  initialValues: {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  },
+});
+
+const onSubmit = handleSubmit((values) => emits('submit:form', values));
+</script>
+
+<template>
+  <div class="flex flex-col gap-6">
+    <Card>
+      <CardHeader class="text-center">
+        <CardTitle class="text-xl"> Register </CardTitle>
+        <CardDescription>
+          Fill in your details below to sign up
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form @submit="onSubmit">
+          <div class="grid gap-6">
+            <FormField v-slot="{ componentField }" name="username">
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    id="username"
+                    type="text"
+                    v-bind="componentField"
+                    required
+                  />
+                </FormControl>
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ componentField }" name="email">
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    id="email"
+                    type="email"
+                    v-bind="componentField"
+                    required
+                  />
+                </FormControl>
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ componentField }" name="password">
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    id="password"
+                    type="password"
+                    v-bind="componentField"
+                    required
+                  />
+                </FormControl>
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ componentField }" name="passwordConfirmation">
+              <FormItem>
+                <FormLabel>Repeat Password</FormLabel>
+                <FormControl>
+                  <Input
+                    id="passwordConfirmation"
+                    type="password"
+                    v-bind="componentField"
+                    required
+                  />
+                </FormControl>
+              </FormItem>
+            </FormField>
+            <Button type="submit" class="w-full"> Create account </Button>
+            <div class="text-center text-sm">
+              Already have an account?
+              <NuxtLink to="/auth/signin" class="underline underline-offset-4">
+                Sign in
+              </NuxtLink>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  </div>
+</template>
