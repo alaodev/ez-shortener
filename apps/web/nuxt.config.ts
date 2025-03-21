e0630c0ce389f945a35c4361acb99fca09212c03
alaodev <alaodev@gmail.com>
2025-03-21 15:50:16 -0300

    chore(apps/web): setup useApi imports

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  imports: {
+    dirs: ['composables/useApi'],
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
