ffc071fb6def2e6fd62d032fddd357f610d41b94
alaodev <alaodev@gmail.com>
2025-03-21 15:57:50 -0300

    chore(apps/web): setup  @pinia/nuxt

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
+    ['@pinia/nuxt', { autoImports: ['defineStore', 'storeToRefs'] }],
     '@nuxtjs/color-mode',
     '@nuxt/eslint',
     '@nuxtjs/tailwindcss',
