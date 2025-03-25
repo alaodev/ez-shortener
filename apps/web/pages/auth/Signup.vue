938226cffba3411b9733961bea22e2c48a936c65
alaodev <alaodev@gmail.com>
2025-03-25 17:18:42 -0300

    feat(apps/web): implements Signup page

---
+++ apps/web/pages/auth/Signup.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import SignupForm, {
+  type SubmitFormDataType,
+} from '@/components/forms/SignupForm.vue';
+import { useToast } from '@/components/ui/toast';
+
+const authStore = useAuthStore();
+
+const { loading } = storeToRefs(authStore);
+
+const { registerUser } = authStore;
+const { toast } = useToast();
+
+async function handleRegisterUser(data: SubmitFormDataType) {
+  await registerUser(data);
+  toast({ title: 'Good news', description: 'User created successfully!' });
+  navigateTo('/auth/signin');
+}
+</script>
+
+<template>
+  <div
+    class="flex min-h-svh flex-col items-center justify-center p-6 md:p-10 md:bg-muted"
+  >
+    <div class="w-full max-w-sm md:max-w-3xl">
+      <SignupForm :disabled="loading" @submit:form="handleRegisterUser" />
+    </div>
+  </div>
+</template>
