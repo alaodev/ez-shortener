6ddaa1bf31dd7e416076c74f7acf4fb0f4f53e4f
alaodev <alaodev@gmail.com>
2025-03-25 11:52:07 -0300

    feat(apps/web): setup useApi error handling

--- apps/web/composables/useApi/handler.ts
+++ apps/web/composables/useApi/handler.ts
@@ -1,3 +1,4 @@
+import { ResponseError } from '@ez-shortener/exceptions';
 import { resolveHeaders } from './headers';
 
 type HandlerOptions<TBody> = {
@@ -13,17 +14,26 @@ export async function apiHandler<TBody = unknown, TResponse = unknown>(
   const { baseUrl: baseURL } = config.public;
   const { body, method } = options;
   const headers = resolveHeaders(path);
-
   try {
     const response = await $fetch<TResponse>(path, {
       baseURL,
       method,
       headers,
       body: body ? JSON.stringify(body) : undefined,
+      onResponseError: async ({ response }) => {
+        if (!response.ok) {
+          if (response._data.name === 'ResponseError') {
+            const { error, message, statusCode }: ResponseError =
+              response._data;
+            throw new ResponseError(statusCode, message, error);
+          }
+          throw new Error('unexpected error');
+        }
+      },
     });
     return response;
   } catch (error) {
-    console.error('API Error:', error);
+    console.error(error);
     throw error;
   }
 }
