e3b5492643bf0964548540fd9dd8b7a9816175e1
alaodev <alaodev@gmail.com>
2025-03-26 17:23:16 -0300

    feat(apps/web): implements ThemeSwitcher component

---
+++ apps/web/components/utils/ThemeSwitcher.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Moon, Sun } from 'lucide-vue-next';
+
+const colorMode = useColorMode();
+
+function switchColorMode() {
+  colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark';
+}
+</script>
+
+<template>
+  <Button size="icon" variant="ghost" @click="switchColorMode">
+    <Moon v-if="colorMode.preference === 'light'" />
+    <Sun v-if="colorMode.preference === 'dark'" />
+  </Button>
+</template>
