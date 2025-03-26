ba834a6a7f10b128507a8af0f842e190bdb2758f
alaodev <alaodev@gmail.com>
2025-03-26 12:42:10 -0300

    chore(apps/web): setup pinia-plugin-persistedstate

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxt/eslint',
     '@nuxtjs/tailwindcss',
+    'pinia-plugin-persistedstate/nuxt',
     'shadcn-nuxt',
   ],
   colorMode: {
