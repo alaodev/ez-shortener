4bd94da299440e75fd57c93ad98de05d671f4950
alaodev <alaodev@gmail.com>
2025-03-26 20:57:43 -0300

    feat(apps/web): includes NavigateToShortenButton on Default layout leading-actions slot

--- apps/web/layouts/Default.vue
+++ apps/web/layouts/Default.vue
@@ -2,12 +2,14 @@
 import LogoutButton from '@/components/utils/LogoutButton.vue';
 import ThemeSwitcher from '@/components/utils/ThemeSwitcher.vue';
 import SidebarToggleButton from '@/components/utils/SidebarToggleButton.vue';
+import NavigateToShortenButton from '@/components/utils/NavigateToShortenButton.vue';
 </script>
 
 <template>
   <Navigation>
     <template #leading-actions>
       <SidebarToggleButton />
+      <NavigateToShortenButton />
     </template>
     <template #actions>
       <ThemeSwitcher />
