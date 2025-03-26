d2bc14aaf144864be028f3d2c1fc88806ba25055
alaodev <alaodev@gmail.com>
2025-03-26 17:00:19 -0300

    feat(apps/web): implements LogoutButton component

---
+++ apps/web/components/utils/LogoutButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { LogOut } from 'lucide-vue-next';
+
+const authStore = useAuthStore();
+
+const { logoutUser } = authStore;
+
+async function handleLogoutUser() {
+  await logoutUser();
+  return navigateTo('/');
+}
+</script>
+
+<template>
+  <Button size="icon" variant="ghost" @click="handleLogoutUser">
+    <LogOut />
+  </Button>
+</template>
