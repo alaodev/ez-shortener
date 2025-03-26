bd082d4207de7f2329bf497976e1f15af194c62b
alaodev <alaodev@gmail.com>
2025-03-26 20:57:02 -0300

    feat(apps/web): includes SidebarToggleButton on Default layout leading-actions slot

--- apps/web/layouts/Default.vue
+++ apps/web/layouts/Default.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import LogoutButton from '@/components/utils/LogoutButton.vue';
 import ThemeSwitcher from '@/components/utils/ThemeSwitcher.vue';
+import SidebarToggleButton from '@/components/utils/SidebarToggleButton.vue';
 </script>
 
 <template>
   <Navigation>
+    <template #leading-actions>
+      <SidebarToggleButton />
+    </template>
     <template #actions>
       <ThemeSwitcher />
       <LogoutButton />
