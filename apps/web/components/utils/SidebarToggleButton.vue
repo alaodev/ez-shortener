c527e0b32ea8065164790e69fd51f69f0952222b
alaodev <alaodev@gmail.com>
2025-03-26 20:50:19 -0300

    feat(apps/web): implements SidebarToggleButton component

---
+++ apps/web/components/utils/SidebarToggleButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { PanelRightOpen, PanelLeftOpen } from 'lucide-vue-next';
+
+const navigationStore = useNavigationStore();
+
+const { sidebarOpen } = storeToRefs(navigationStore);
+
+const { toggleSidebar } = navigationStore;
+</script>
+
+<template>
+  <Button size="icon" variant="ghost" @click="toggleSidebar">
+    <PanelRightOpen v-if="sidebarOpen" />
+    <PanelLeftOpen v-else />
+  </Button>
+</template>
