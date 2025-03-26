daf6c749afdfb2860c39cac9b3d6f699419da127
alaodev <alaodev@gmail.com>
2025-03-26 20:51:57 -0300

    feat(apps/web): implements NavigateToShortenButton component

---
+++ apps/web/components/utils/NavigateToShortenButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { FormInput } from 'lucide-vue-next';
+</script>
+
+<template>
+  <Button size="icon" variant="ghost" @click="navigateTo('/shorten')">
+    <FormInput />
+  </Button>
+</template>
