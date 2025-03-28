d174408d9a5d43992dd236386fc694f5e1553e7d
alaodev <alaodev@gmail.com>
2025-03-28 12:45:31 -0300

    feat(apps/web): implements NavigateToButton component

---
+++ apps/web/components/utils/NavigateToButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+export type Props = {
+  to: string;
+  icon: Component;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <Button size="icon" variant="ghost" @click="navigateTo(to)">
+    <component :is="icon" />
+  </Button>
+</template>
