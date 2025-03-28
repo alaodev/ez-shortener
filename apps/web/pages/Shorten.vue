a2daf02f4a981ca167923cb98498916603164a6d
alaodev <alaodev@gmail.com>
2025-03-28 11:55:48 -0300

    feat(apps/web): implements shortenUrl on Shorten page

--- apps/web/pages/Shorten.vue
+++ apps/web/pages/Shorten.vue
@@ -1,11 +1,37 @@
 <script setup lang="ts">
+import ShortenForm, {
+  type SubmitFormDataType,
+} from '@/components/forms/ShortenForm.vue';
+
 definePageMeta({
   auth: true,
   name: 'shorten',
   path: '/shorten',
 });
+
+const urlsStore = useUrlsStore();
+
+const { loading, shortenedUrl } = storeToRefs(urlsStore);
+
+const { shortenUrl, resetStore } = urlsStore;
+
+async function handleShortenUrl(data: SubmitFormDataType) {
+  const { originalUrl } = data;
+  const { domainUrl } = useRuntimeConfig().public;
+  const createdUrl = await shortenUrl(originalUrl);
+  shortenedUrl.value = `${domainUrl}${createdUrl.shortId}`;
+}
 </script>
 
 <template>
-  <h1>Shorten</h1>
+  <div class="flex flex-col items-center justify-center h-full">
+    <div class="max-w-xl w-10/12">
+      <ShortenForm
+        :shortened-url
+        :loading
+        @submit:form="handleShortenUrl"
+        @reset:form="resetStore"
+      />
+    </div>
+  </div>
 </template>
