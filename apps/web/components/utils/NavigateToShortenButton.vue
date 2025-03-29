bd8df81a05bfee4486c225614ae470e3fcf8a980
alaodev <alaodev@gmail.com>
2025-03-29 14:07:00 -0300

    chore(apps/web): removes unnecessary component

--- apps/web/components/utils/NavigateToShortenButton.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { FormInput } from 'lucide-vue-next';
-</script>
-
-<template>
-  <Button size="icon" variant="ghost" @click="navigateTo('/shorten')">
-    <FormInput />
-  </Button>
-</template>
