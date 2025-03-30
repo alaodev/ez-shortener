aa6639b9f9244a3690c24d0882a9a03bd27b647e
alaodev <alaodev@gmail.com>
2025-03-30 13:24:13 -0300

    fix(apps/web): redirects user to correct authenticated route

--- apps/web/middleware/auth.global.ts
+++ apps/web/middleware/auth.global.ts
@@ -1,6 +1,8 @@
 export default defineNuxtRouteMiddleware(async (to) => {
+  const authPages = ['/', '/signup'];
   const authStore = useAuthStore();
   const { authenticated } = storeToRefs(authStore);
   if (!authenticated.value && to.meta.auth) return navigateTo('/');
-  if (authenticated.value && !to.meta.auth) return navigateTo('/dashboard');
+  if (authenticated.value && authPages.includes(to.path))
+    return navigateTo('/dashboard');
 });
