6f5e8bf40b3aa7d826be4e1aad779bcf729fdc6e
alaodev <alaodev@gmail.com>
2025-03-29 22:42:26 -0300

    feat(apps/web): implements ShortenedUrlsActions component

---
+++ apps/web/layouts/components/ShortenedUrlsActions.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { MoreHorizontal, Trash2 } from 'lucide-vue-next';
+import ShortenedUrlsDeleteModal from './ShortenedUrlsDeleteModal.vue';
+
+export type Props = {
+  shortenedUrlId: string;
+};
+
+defineProps<Props>();
+
+const openDeleteModal = ref(false);
+
+function handleDeleteModalStatus(status: boolean) {
+  openDeleteModal.value = status;
+}
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <SidebarMenuAction>
+        <MoreHorizontal />
+      </SidebarMenuAction>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent side="right" align="start">
+      <DropdownMenuItem
+        class="text-destructive"
+        @click="handleDeleteModalStatus(true)"
+      >
+        <Trash2 />
+        <span>Delete</span>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+  <ShortenedUrlsDeleteModal
+    :open="openDeleteModal"
+    :shortened-url-id
+    @close:modal="handleDeleteModalStatus(false)"
+  />
+</template>
