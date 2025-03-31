e73564312a1f412fba5ab2ff9861a46cbec93023
alaodev <alaodev@gmail.com>
2025-03-31 19:07:40 -0300

    feat(apps/urls): implements UrlSchema pre findOneAndDelete

--- apps/urls/src/modules/urls/urls.module.ts
+++ apps/urls/src/modules/urls/urls.module.ts
@@ -1,6 +1,15 @@
-import { MongooseModule } from '@ez-shortener/databases/nestjs-mongoose';
+import {
+  getModelToken,
+  MongooseModule,
+} from '@ez-shortener/databases/nestjs-mongoose';
 import { Module } from '@nestjs/common';
-import { Url, UrlSchema, Access, AccessSchema } from './infrastructure/schemas';
+import {
+  Url,
+  UrlSchema,
+  Access,
+  AccessDocument,
+  AccessSchema,
+} from './infrastructure/schemas';
 import {
   gatewayProviders,
   repositoryProviders,
@@ -8,12 +17,35 @@ import {
   usecaseProviders,
 } from './infrastructure/providers';
 import { UrlsController } from './presentation/controller/urls.controller';
+import { ObjectId, Model } from '@ez-shortener/databases/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: Access.name, schema: AccessSchema, collection: 'access' },
-      { name: Url.name, schema: UrlSchema, collection: 'urls' },
+    MongooseModule.forFeatureAsync([
+      {
+        name: Access.name,
+        useFactory: function () {
+          const schema = AccessSchema;
+          return schema;
+        },
+        collection: 'access',
+      },
+      {
+        name: Url.name,
+        useFactory: (accessModel: Model<AccessDocument>) => {
+          const schema = UrlSchema;
+          schema.pre('findOneAndDelete', async function (next) {
+            const urlId = this.getQuery()._id as ObjectId;
+            await accessModel.deleteMany({
+              url: urlId,
+            });
+            next();
+          });
+          return schema;
+        },
+        inject: [getModelToken(Access.name)],
+        collection: 'urls',
+      },
     ]),
   ],
   controllers: [UrlsController],
