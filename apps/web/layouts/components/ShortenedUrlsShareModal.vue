9ab09b3cfcf744041a6c06f2492be479d3b7f8b6
alaodev <alaodev@gmail.com>
2025-04-01 08:46:45 -0300

    feat(apps/web): implements ShortenedUrlsShareModal component

---
+++ apps/web/layouts/components/ShortenedUrlsShareModal.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { AlertDialogProps } from 'reka-ui';
+import ShortenInput from '@/components/inputs/ShortenInput.vue';
+
+export type Props = AlertDialogProps & {
+  shortenedUrl: string;
+};
+
+const props = defineProps<Props>();
+const emits = defineEmits<{
+  (event: 'close:modal'): void;
+}>();
+
+function handleCancel() {
+  emits('close:modal');
+}
+</script>
+
+<template>
+  <AlertDialog v-bind="props">
+    <AlertDialogContent @interact-outside="handleCancel">
+      <AlertDialogHeader>
+        <AlertDialogTitle>Share your shortened link</AlertDialogTitle>
+        <AlertDialogDescription>
+          Click the button to copy your link to the clipboard.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <ShortenInput :shortened-url />
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
