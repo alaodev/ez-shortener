9691180bda8126373e4c02889e67fc125e5fe047
alaodev <alaodev@gmail.com>
2025-03-31 21:30:41 -0300

    feat(apps/web): implements ShortenInput component

--- apps/web/components/forms/ShortenForm.vue
+++ apps/web/components/forms/ShortenForm.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 import * as zod from 'zod';
-import { toTypedSchema } from '@vee-validate/zod';
-import { useClipboard } from '@vueuse/core';
-import { useToast } from '@/components/ui/toast';
-import { useForm } from 'vee-validate';
-import { ArrowRight, Clipboard, Loader2, RefreshCcw } from 'lucide-vue-next';
+import ShortenInput from '../inputs/ShortenInput.vue';
 
 export type SubmitFormDataType = zod.infer<typeof shortenSchemaValidator>;
 export type Props = {
@@ -18,114 +14,43 @@ const emits = defineEmits<{
   (event: 'reset:form'): void;
 }>();
 
+const originalUrl = ref('');
+
 const shortenSchemaValidator = zod.object({
   originalUrl: zod.string().url().nonempty(),
 });
-
-const formSchema = toTypedSchema(shortenSchemaValidator);
-
-const { handleSubmit, resetForm, values } = useForm({
-  validationSchema: formSchema,
-  initialValues: {
-    originalUrl: '',
-  },
-});
-const { copy } = useClipboard();
-const { toast } = useToast();
 
 const formTitle = computed(() => {
   if (props.loading) return 'Shortening...';
   if (!props.shortenedUrl) return 'Which link to shorten?';
   return 'Link successfully shortened!';
 });
-const shrinkStyle = computed(() =>
-  props.loading ? 'w-[3.5rem] justify-end pr-2.5' : 'w-full pl-5 pr-2 py-2',
-);
 
-const onSubmit = handleSubmit((values) => emits('submit:form', values));
-
-async function handleCopyToClipboard() {
-  if (!props.shortenedUrl) return;
-  await copy(props.shortenedUrl);
-  toast({
-    title: 'Good news',
-    description: 'Shortened link copied to clipboard',
-  });
+function handleResetInput() {
+  originalUrl.value = '';
+  emits('reset:form');
 }
 
-function handleReset() {
-  resetForm();
-  emits('reset:form');
+function handleSubmitForm() {
+  const { error } = shortenSchemaValidator.safeParse({
+    originalUrl: originalUrl.value,
+  });
+  if (error) return;
+  emits('submit:form', { originalUrl: originalUrl.value });
 }
 </script>
 
 <template>
-  <form class="flex flex-col gap-4 items-center w-full" @submit="onSubmit">
+  <form
+    class="flex flex-col gap-4 items-center w-full"
+    @submit.prevent="handleSubmitForm"
+  >
     <h1 class="font-medium text-2xl">{{ formTitle }}</h1>
-    <div
-      class="flex items-center gap-4 rounded-full duration-300 shadow-lg dark:bg-secondary h-[3.5rem]"
-      :class="shrinkStyle"
-    >
-      <div v-if="!loading" class="flex-1 min-w-0">
-        <FormField
-          v-if="!shortenedUrl"
-          v-slot="{ componentField }"
-          name="originalUrl"
-        >
-          <FormItem>
-            <FormControl>
-              <input
-                id="originalUrl"
-                v-bind="componentField"
-                class="bg-transparent outline-none w-full"
-                type="url"
-                placeholder="Type your link..."
-                autocomplete="off"
-              />
-            </FormControl>
-          </FormItem>
-        </FormField>
-        <span
-          v-else
-          class="block overflow-hidden text-ellipsis whitespace-nowrap"
-        >
-          {{ shortenedUrl }}
-        </span>
-      </div>
-      <Button
-        v-if="shortenedUrl && !loading"
-        class="rounded-full"
-        size="icon"
-        variant="ghost"
-        @click="handleReset"
-      >
-        <RefreshCcw />
-      </Button>
-      <Button
-        v-if="loading"
-        class="rounded-full flex-shrink-0"
-        size="icon"
-        variant="ghost"
-      >
-        <Loader2 class="animate-spin" />
-      </Button>
-      <Button
-        v-if="!loading && !shortenedUrl"
-        class="rounded-full flex-shrink-0"
-        size="icon"
-        :disabled="!values.originalUrl"
-      >
-        <ArrowRight />
-      </Button>
-      <Button
-        v-if="!loading && shortenedUrl"
-        class="rounded-full flex-shrink-0"
-        size="icon"
-        :disabled="!shortenedUrl"
-        @click="handleCopyToClipboard"
-      >
-        <Clipboard />
-      </Button>
-    </div>
+    <ShortenInput
+      v-model="originalUrl"
+      :shortened-url="props.shortenedUrl"
+      :loading="props.loading"
+      @reset:input="handleResetInput"
+    />
   </form>
 </template>
---
+++ apps/web/components/inputs/ShortenInput.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { ArrowRight, Clipboard, Loader2, RefreshCcw } from 'lucide-vue-next';
+import { useClipboard, useVModel } from '@vueuse/core';
+import { Button } from '@/components/ui/button';
+import { useToast } from '@/components/ui/toast';
+
+export type Props = {
+  modelValue?: string;
+  shortenedUrl?: string;
+  loading?: boolean;
+};
+
+const props = defineProps<Props>();
+const emits = defineEmits<{
+  (event: 'reset:input' | 'submit:input'): void;
+  (event: 'update:modelValue'): string;
+}>();
+
+const { copy } = useClipboard();
+const { toast } = useToast();
+
+const value = useVModel(props, 'modelValue', emits);
+
+const shrinkStyle = computed(() =>
+  props.loading ? 'w-[3.5rem] justify-end pr-2.5' : 'w-full pl-5 pr-2 py-2',
+);
+
+async function handleCopyToClipboard() {
+  if (!props.shortenedUrl) return;
+  await copy(props.shortenedUrl);
+  toast({
+    title: 'Good news',
+    description: 'Shortened link copied to clipboard',
+  });
+}
+</script>
+
+<template>
+  <div
+    class="flex items-center gap-4 rounded-full duration-300 shadow-lg dark:bg-secondary h-[3.5rem]"
+    :class="shrinkStyle"
+  >
+    <div v-if="!props.loading" class="flex-1 min-w-0">
+      <span
+        v-if="props.shortenedUrl"
+        class="block overflow-hidden text-ellipsis whitespace-nowrap"
+      >
+        {{ props.shortenedUrl }}
+      </span>
+      <input
+        v-else
+        id="originalUrl"
+        v-model="value"
+        class="bg-transparent outline-none w-full"
+        type="url"
+        placeholder="Type your link..."
+        autocomplete="off"
+        @keyup.enter="emits('submit:input')"
+      />
+    </div>
+    <Button
+      v-if="props.shortenedUrl && !props.loading"
+      class="rounded-full"
+      size="icon"
+      variant="ghost"
+      @click="emits('reset:input')"
+    >
+      <RefreshCcw />
+    </Button>
+    <Button
+      v-if="props.loading"
+      class="rounded-full flex-shrink-0"
+      size="icon"
+      variant="ghost"
+    >
+      <Loader2 class="animate-spin" />
+    </Button>
+    <Button
+      v-if="!props.loading && !props.shortenedUrl"
+      class="rounded-full flex-shrink-0"
+      size="icon"
+      :disabled="!value"
+      @click="emits('submit:input')"
+    >
+      <ArrowRight />
+    </Button>
+    <Button
+      v-if="!props.loading && props.shortenedUrl"
+      class="rounded-full flex-shrink-0"
+      size="icon"
+      :disabled="!props.shortenedUrl"
+      @click.prevent="handleCopyToClipboard"
+    >
+      <Clipboard />
+    </Button>
+  </div>
+</template>
