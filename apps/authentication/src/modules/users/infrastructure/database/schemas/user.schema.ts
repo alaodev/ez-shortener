68d1550752c355b82c872872cdba91663d2f4984
alaodev <alaodev@gmail.com>
2025-04-01 18:21:52 -0300

    chore(apps/authentication): defines UserSchema explicit timestamp fields

--- apps/authentication/src/modules/users/infrastructure/database/schemas/user.schema.ts
+++ apps/authentication/src/modules/users/infrastructure/database/schemas/user.schema.ts
@@ -17,6 +17,12 @@ export class User {
 
   @Prop({ required: true })
   password: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
