2687a3be7b41a260cfde81e6b90fb67de9ac471c
alaodev <alaodev@gmail.com>
2025-04-01 18:27:38 -0300

    chore(apps/authentication): defines UserSchema on repository model

--- apps/authentication/src/modules/users/infrastructure/repositories/mongo-create-user.repository.ts
+++ apps/authentication/src/modules/users/infrastructure/repositories/mongo-create-user.repository.ts
@@ -2,15 +2,17 @@ import { Model } from '@ez-shortener/databases/mongoose';
 import { InjectModel } from '@ez-shortener/databases/nestjs-mongoose';
 import { Injectable } from '@nestjs/common';
 import { User as UserSchema } from '../database/schemas/user.schema';
-import { User } from '../../domain/entities/user.entity';
+import { User as UserEntity } from '../../domain/entities/user.entity';
 import { CreateUserRepository } from '../../domain/repositories/create-user.repository';
 import { CreateUserRepositoryOutput } from '../../domain/types/outputs/repositories/create-user-repository.output';
 
 @Injectable()
 export class MongoCreateUserRepository implements CreateUserRepository {
-  constructor(@InjectModel(UserSchema.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(UserSchema.name) private userModel: Model<UserSchema>,
+  ) {}
 
-  async createUser(user: User): Promise<CreateUserRepositoryOutput> {
+  async createUser(user: UserEntity): Promise<CreateUserRepositoryOutput> {
     const createdUser = await this.userModel.create(user);
     return {
       id: createdUser._id.toString(),
