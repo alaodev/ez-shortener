57f16f8a376f198d16ed8f5d71f12a002fc7c83b
alaodev <alaodev@gmail.com>
2025-04-03 17:15:52 -0300

    feat(apps/web): implements AccessTable on Dashboard

--- apps/web/pages/Dashboard.vue
+++ apps/web/pages/Dashboard.vue
@@ -1,11 +1,45 @@
 <script setup lang="ts">
+import AccessTable, {
+  type AccessData,
+} from '@/components/tables/AccessTable.vue';
+
 definePageMeta({
   auth: true,
   name: 'dashboard',
   path: '/dashboard',
 });
+
+const accessStore = useAccessStore();
+
+const { loadingAccess, access } = storeToRefs(accessStore);
+
+const accessTableData: ComputedRef<AccessData[]> = computed(() =>
+  access.value.map((item) => ({
+    originalUrl: item.url.originalUrl,
+    address: item.address,
+    browserName: item.browserName,
+    browserVersion: item.browserVersion,
+    osName: item.osName,
+    osVersion: item.osVersion,
+    createdAt: new Date(item.createdAt),
+  })),
+);
+
+const { getAccess } = accessStore;
+
+async function handleGetAccess() {
+  await useAsyncData('access', async () => {
+    return getAccess();
+  });
+}
+
+handleGetAccess();
 </script>
 
 <template>
-  <h1>Dashboard</h1>
+  <div class="flex">
+    <div class="container mx-auto">
+      <AccessTable v-if="!loadingAccess" :data="accessTableData" />
+    </div>
+  </div>
 </template>
