9bbba2def2962f7cc49333c433a73d6471fdde69
alaodev <alaodev@gmail.com>
2025-04-15 11:02:54 -0300

    feat(apps/web): implements ShortenUrlsList loading skeleton

--- apps/web/layouts/components/ShortenedUrlsList.vue
+++ apps/web/layouts/components/ShortenedUrlsList.vue
@@ -4,7 +4,7 @@ import ShortenedUrlsActions from './ShortenedUrlsActions.vue';
 const runtimeConfig = useRuntimeConfig();
 const urlsStore = useUrlsStore();
 
-const { urls } = storeToRefs(urlsStore);
+const { loadingGetUrls, urls } = storeToRefs(urlsStore);
 const { domainUrl } = runtimeConfig.public;
 
 const shortenedUrls = computed(() =>
@@ -20,22 +20,33 @@ const shortenedUrls = computed(() =>
   <SidebarGroup>
     <SidebarGroupContent>
       <SidebarMenu v-auto-animate="{ duration: 1000 }">
-        <SidebarMenuItem
-          v-for="shortenedUrl of shortenedUrls"
-          :key="shortenedUrl.id"
-        >
-          <SidebarMenuButton as-child>
-            <NuxtLink>
-              <span class="overflow-hidden text-ellipsis whitespace-nowrap">
-                {{ shortenedUrl.originalUrl }}
-              </span>
-            </NuxtLink>
-          </SidebarMenuButton>
-          <ShortenedUrlsActions
-            :shortened-url-id="shortenedUrl.id"
-            :shortened-url="shortenedUrl.shortenedUrl"
-          />
-        </SidebarMenuItem>
+        <template v-if="!loadingGetUrls">
+          <SidebarMenuItem
+            v-for="shortenedUrl of shortenedUrls"
+            :key="shortenedUrl.id"
+          >
+            <SidebarMenuButton as-child>
+              <NuxtLink>
+                <span class="overflow-hidden text-ellipsis whitespace-nowrap">
+                  {{ shortenedUrl.originalUrl }}
+                </span>
+              </NuxtLink>
+            </SidebarMenuButton>
+            <ShortenedUrlsActions
+              :shortened-url-id="shortenedUrl.id"
+              :shortened-url="shortenedUrl.shortenedUrl"
+            />
+          </SidebarMenuItem>
+        </template>
+        <template v-else>
+          <div class="flex flex-col gap-2">
+            <Skeleton
+              v-for="item in 5"
+              :key="item"
+              class="h-7 rounded-md w-full"
+            />
+          </div>
+        </template>
       </SidebarMenu>
     </SidebarGroupContent>
   </SidebarGroup>
