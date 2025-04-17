2942dfeaa2ffb2258f2869e07cea19d258705a61
alaodev <alaodev@gmail.com>
2025-04-17 12:01:40 -0300

    feat(apps/urls): enables nest express trust proxy

--- apps/urls/src/main.ts
+++ apps/urls/src/main.ts
@@ -2,6 +2,7 @@ import * as cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from '@ez-shortener/exceptions';
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,9 +12,10 @@ async function bootstrap() {
     logger.error('MONGO_USERS_URI must be provided');
     process.exit(1);
   }
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(cookieParser());
+  app.set('trust proxy', true);
   await app.listen(port, () => logger.log(`Server running on port ${port}`));
 }
 bootstrap();
